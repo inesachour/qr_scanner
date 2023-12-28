0beba8319ba0b8568043f7749aacdd0051be2dae
inesa <ines.achour@insat.ucar.tn>
2023-12-29 00:19:45 +0100

    Setup qr scanner

---
+++ lib/screens/qr_scanner.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_scanner/screens/scan_history.dart';
+
+class Scanner extends StatefulWidget {
+  const Scanner({Key? key}) : super(key: key);
+
+  @override
+  State<Scanner> createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black.withOpacity(0.5),
+        appBar: AppBar(
+          title: Text("Scanner", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+        ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              MobileScanner(
+                allowDuplicates: false,
+                onDetect: onDetectBarcode,
+              ),
+
+              Positioned(
+                bottom: 10,
+                right: 10,
+                child: ElevatedButton(
+                  child: const Icon(Icons.history),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ScanHistoryScreen()),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+
+  void onDetectBarcode(Barcode barcode, MobileScannerArguments? args){
+    print(barcode);
+  }
+
+}
---
+++ lib/screens/scan_history.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ScanHistoryScreen extends StatefulWidget {
+  const ScanHistoryScreen({super.key});
+
+  @override
+  State<ScanHistoryScreen> createState() => _ScanHistoryScreenState();
+}
+
+class _ScanHistoryScreenState extends State<ScanHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: SafeArea(
+        child: Container(
+        ),
+      ),
+    );
+  }
+}
